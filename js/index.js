a1311adde14c895fe82385a390d7262200e10c9b
Eyþór Magnússon <eysi09@gmail.com>
2017-09-12 10:32:09 +0200

    Add add/sub operations

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('convert-btn').addEventListener('click', convertTime);
+  document.getElementById('op-select').addEventListener('change', updatePlaceholder);
 
   function convertTime() {
     const input = document.getElementById('time-input').value;
-    const op = document.getElementById('operator').value;
-    const val = document.getElementById('value').value;
-    const [h, m, s, ms] = input.split(':');
-    const d = moment
-      .duration(parseInt(h), 'hours')
-      .add(parseInt(m), 'minutes')
-      .add(parseInt(s), 'seconds')
-      .add(parseInt(ms));
-    const totalMs = d.asMilliseconds();
+    const op = document.getElementById('op-select').value;
+    const val = document.getElementById('val-input').value;
+    const totalMs = toMs(input);
     const calcedD = moment.duration(calc(totalMs, val, op));
     const ans = calcedD.days() +
       ' DAYS ' + calcedD.hours() +
@@ -24,13 +19,39 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function calc(a, b, op) {
-    if (op === '*') {
-      return a*b;
+    if (op === 'mult') {
+      return a*parseFloat(b);
     }
-    if (op === '/') {
-      return a/b;
+    if (op === 'div') {
+      return a/parseFloat(b);
     }
-    return null;
+    if (op === 'add') {
+      return a+toMs(b);
+    }
+    if (op === 'sub') {
+      return a-toMs(b);
+    }
+    return 0;
+  }
+
+  function updatePlaceholder(event) {
+    const { value } = event.target;
+    const valInput = document.getElementById('val-input');
+    if (value === 'mult' || value === 'div') {
+      valInput.placeholder = "2.5";
+    } else {
+      valInput.placeholder = "hh:mm:ss:ms";
+    }
+  }
+
+  function toMs(input) {
+    const [h, m, s, ms] = input.split(':');
+    const d = moment
+      .duration(parseInt(h), 'hours')
+      .add(parseInt(m), 'minutes')
+      .add(parseInt(s), 'seconds')
+      .add(parseInt(ms));
+    return d.asMilliseconds();
   }
 
 });
